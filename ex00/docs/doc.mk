82e762d45a975bebead4b35fb3c2050e0a1c0f17
tobeshota <Shota.Tobe@keio.jp>
2024-09-24 14:58:33 +0900

    feat: modify `make doc` rule

--- ex00/docs/doc.mk
+++ ex00/docs/doc.mk
@@ -7,7 +7,6 @@ DOXYFILE_PATH	= Doxyfile
 INDEX_HTML_PATH	= $(HTML_DIR)index.html
 
 doc:
-	@ if [ ! -f $(DOXYFILE_PATH) ]; then doxygen -g $(DOXYFILE_PATH); fi
 	@ doxygen $(DOXYFILE_PATH)
 	@ open $(INDEX_HTML_PATH)
 
